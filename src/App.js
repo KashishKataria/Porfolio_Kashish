3d8679241d387b8309346e94a89486fbe5ed29a4
KashishKataria <141554217+KashishKataria@users.noreply.github.com>
2023-11-13 23:13:33 -0800

    Tension not

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import { Header } from './components/Header';
-import { HeroSection } from './components/HeroSection';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import React from 'react';
+import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
+import Auction from './components/Auction';
+import { DiscoverFunction, anotherFunction } from './components/DiscoverFunctions';
 
 function App() {
+
+  DiscoverFunction();
+  anotherFunction();
   return (
-    <>
-    <Header/>
-    <HeroSection/>
-    <Footer/>
-    </>
+    <div className="App">
+      <Navbar></Navbar>
+      <Welcome></Welcome>
+      <Auction></Auction>
+      <Header></Header>
+      <Hero></Hero>
+      <Footer></Footer>
+    </div>
   );
 }
 
--- src/components/HeroSection.js
+++
@@ -1,7 +0,0 @@
-export const HeroSection = () => {
-    return(
-        <>
-        <div>HeroSection</div>
-        </>
-    )
-}
